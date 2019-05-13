86819616e0ca43514efa113dbeceffbb7aabb616
Ferveloper <fermervil@gmail.com>
2019-05-13 23:43:47 +0200

    Create Post model

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+    TECHNOLOGY = 'TEC'
+    STARTUPS = 'STA'
+    HEALTH = 'HEA'
+    POLITICS = 'POL'
+
+    CATEGORIES = [
+        [TECHNOLOGY, 'Tecnología'],
+        [STARTUPS, 'Startups'],
+        [HEALTH, 'Salud'],
+        [POLITICS, 'Politica']
+    ]
+
+    title = models.CharField(max_length=150)
+    url = models.URLField()
+    introduction = models.TextField()
+    body = models.TextField()
+    publication_date = models.DateTimeField()
+    creation_date = models.DateTimeField(auto_now_add=True)
+    modification_date = models.DateTimeField(auto_now=True)
+    categories = models.CharField(max_length=3, choices=CATEGORIES)
+    # author = models.CharField(max_length=30)
+
+    def __str__(self):
+        return self.name
